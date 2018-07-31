781312a45784f349da5ea52b64d018750551b7ae
Nguyen Duc Anh <ducanh7897@gmail.com>
2018-07-31 15:03:13 +0700

    show update edit fix 1 fix 2 fix 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  before_action :logged_in_user, except: %i(new show create)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+  before_action :load_user, except: %i(index new create)
+
+  def index
+    @users = User.all.page(params[:page]).per Settings.number_page
   end
 
-  def show
-    @user = User.find_by id: params[:id]
+  def show; end
+
+  def new
+    @user = User.new
   end
 
   def create
@@ -18,9 +25,57 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-  def user_params
-    params.require(:user).permit :name, :email, :password, :password_confirmation
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".updated"
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".deleted"
+    else
+      flash[:danger] = t ".undelete"
+    end
+      redirect_to users_url
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit :name, :email, :password,
+        :password_confirmation
+    end
+
+    def logged_in_user
+      return if logged_in?
+      store_location
+      flash[:danger] = t ".please"
+      redirect_to login_url
+    end
+
+    def correct_user
+      @user = User.find_by id: params[:id]
+      return if current_user? @user
+      flash[:danger] = t ".incorrect"
+      redirect_to root_url
+    end
+
+    def admin_user
+      return if current_user.admin?
+      flash[:danger] = t ".user"
+      redirect_to root_url
+    end 
+
+    def load_user
+      @user = User.find_by id: params[:id]
+      return if @user
+      flash[:danger] = t ".failuser"
+      redirect_to root_url
+    end
 end
