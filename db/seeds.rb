c09c1f6070aab86cc6442973ed2ac40c54213c9f
Nguyen Duc Anh <ducanh7897@gmail.com>
2018-08-07 09:47:10 +0700

    follow fix1

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,10 @@ User.create! name: "Example User", email: "example@railstutorial.org",
     password_confirmation: password, activated: true,
     activated_at: Time.zone.now
 end
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
